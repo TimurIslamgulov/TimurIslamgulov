b0348cffc8303545fa8709c3539fda987716a33c
Alexandro <scherbakov857@gmail.com>
2022-09-12 10:57:07 +0500

    Add DZ_1_2

---
+++ DZ_1_2/DZ_1/Program.cs
@@ -0,0 +1,75 @@
+﻿//Задача №8
+/*Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int M = 1;
+while(M < N)
+{
+    if(M%2 == 0)
+    {
+        Console.Write(M + ",");
+    }
+    M++;
+    
+}
+*/
+
+
+
+//Задача №6
+/*Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+if(N%2 == 0)
+{
+    Console.WriteLine(N + " - чётное число");
+}
+
+else
+{
+   Console.WriteLine(N + " - нечётное число!!!"); 
+}
+*/
+
+
+
+//Задача №4
+/*Console.WriteLine("Введите три числа: ");
+int number_1 = int.Parse(Console.ReadLine());
+int number_2 = int.Parse(Console.ReadLine());
+int number_3 = int.Parse(Console.ReadLine());
+int max = number_1;
+if(max < number_2)
+{
+   max = number_2 ;
+}
+
+if(max < number_3)
+{
+   max = number_3;
+}
+Console.WriteLine("Максимальное число - " + max);
+*/
+
+
+//Задача №2
+/*Console.WriteLine("Дано два числа ");
+int number_1 = new Random().Next(1,20);
+int number_2 = new Random().Next(1,20);
+Console.WriteLine(number_1);
+Console.WriteLine(number_2);
+
+
+if(number_1 > number_2)
+{
+    Console.WriteLine("Максимальное число - " + number_1);
+    Console.WriteLine("Минимальное число - " + number_2);
+}
+else
+{
+    Console.WriteLine("Максимальное число - " + number_2);
+    Console.WriteLine("Минимальное число - " + number_1);
+}
+*/
+
+
+
---
+++ DZ_1_2/DZ_2/Program.cs
@@ -0,0 +1,120 @@
+﻿//Нахождение максимума любого размера одномерного массива
+/*
+void FillArray(int[] collection, int N)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        
+        collection[index] = new Random().Next(1, N);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+    Console.WriteLine( "/");
+
+}
+void Max(int[] max)
+{
+    int leght = max.Length;
+    int position = 1;
+    int maximum = 0;
+    while(leght > position)
+    {
+        if(max[maximum] < max[position])
+        {
+            max[maximum] = max[position];
+        }
+        position++;
+
+    }
+    Console.WriteLine("Максимальное число - " + max[maximum]);
+}
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N];
+FillArray(array, N);
+PrintArray(array);
+Max(array);
+*/
+
+
+//Задача 10 & 13
+/*void Num_2_3(int number)
+{
+    if(number/100 > 9 || number/10 < 9 )
+    {
+        Console.WriteLine("Просили же трёхзначное число!!!!");
+        
+    }
+    else
+    {
+        int num_2 = number / 10 % 10;
+        int num_3 = number % 100 % 10;
+        Console.WriteLine("Вторая цифра - " + num_2);
+        Console.WriteLine("Третья цифра - " + num_3);
+
+    }
+}
+Console.Write("Введите трёхзначное число - ");
+int number = int.Parse(Console.ReadLine());
+Num_2_3(number);
+*/
+
+
+
+// Задача 15
+/*
+void data(int number)
+{
+    if(number < 8)
+    {
+        if(number == 6 || number == 7)
+        {
+            Console.WriteLine("Чтож сегодня выходной и можно сходить в парк");
+        }
+        else
+        {
+            Console.WriteLine("Сегодня рабочий день, стоит ударно поработать"); 
+        }
+    }
+    else Console.WriteLine("Введите число от 1 до 7!!!!");
+}
+Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+data(number);
+*/
+
+
+// задача необязательная
+/*void robot (int word)
+{
+    if(word%10 == 1 && word % 100 != 11)
+    {
+        Console.WriteLine(word + " программист. ");
+    }
+    else
+    {
+        if(word % 10 == 2 && word % 100 != 12 || word % 10 == 3 && word % 100 != 13 || word % 10 == 4 && word % 100 != 14)
+        {
+            Console.WriteLine(word + " программистa. ");
+        }
+        else
+        {
+            Console.WriteLine(word + " программистов. ");
+        }
+    }
+}
+Console.WriteLine("Введите кол-во студентов в классе: ");
+int word = int.Parse(Console.ReadLine());
+robot(word);
+*/
